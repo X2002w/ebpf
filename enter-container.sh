711d1a494848fe07bb6a60e857d6ec9072e344d0
X2002w <15008695828@163.com>
2026-07-12 03:00:39 +0800

    feat: enter-container.sh 添加 --pull 拉取最新镜像

--- enter-container.sh
+++ enter-container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # 确保容器已启动
-sudo docker compose up -d
+sudo docker compose build --pull && sudo docker compose up -d
 
 # 进入容器并切换到挂载的工作目录
 sudo docker compose exec openkylin bash -c "cd /workspace && exec bash"
